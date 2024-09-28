4783d3bd95f01b1228ac9f28b23dfb0374e4206e
lwyers2 <lwyers2@gmail.com>
2024-09-28 17:15:16 +0100

    Part 4-1

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,35 +1,20 @@
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-
-//DO NOT SAVE YOUR PASSWORD TO GITHUB!!!
-const url = process.env.MONGODB_URI
-
-
-mongoose.connect(url)
-    .then(result => {
-        console.log("connected to MongoDB");
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message);
-
-    })
-
 
 const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minLength: 5,
-      required: true
-    },
-    important: Boolean,
-  })
-  
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Note', noteSchema)
