ca21148d552e1cd52e706045f74c16307d220a63
lwyers2 <lwyers2@gmail.com>
2024-09-28 12:05:38 +0100

    Deploying the database backend to production - Section 3d

--- backend/index.js
+++ backend/index.js
@@ -30,6 +30,8 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError') {
     return response.status(400).send({ error : 'malformed id'})
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
   next(error)
 }
@@ -45,14 +47,8 @@ app.get('/api/notes', (request, response) => {
 
 
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
 
   const note = new Note({
     content: body.content,
@@ -62,6 +58,7 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
@@ -85,14 +82,14 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes:id', (request, response, next) => {
-  const body = request.body
+  const {content, important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, {new:true})
+ 
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+     {new:true, runValidators: true, context: 'query'}
+    )
   .then(updatedNote => {
     response.json(updatedNote)
   })
