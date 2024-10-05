a4bf2f881f3c8c05e952c79ccb609802c753b0d1
lwyers2 <lwyers2@gmail.com>
2024-10-05 13:14:37 +0100

    4-c

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,8 +1,11 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note
+    .find({}).populate('user', { username: 1 , name: 1 })
+
   response.json(notes)
 })
 
@@ -21,12 +24,19 @@ notesRouter.put('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  //user also has to be updated
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
